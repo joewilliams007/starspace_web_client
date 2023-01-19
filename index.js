69dbc697ea6006c61e08cf2e71071f2746519428
Universe <williamsthehack@gmail.com>
2023-01-19 20:07:36 +0100

    up

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express()
+const PORT = 7788;
+const path = require('path');
+    var fs = require('fs')
+app.use(express.static(path.join(__dirname,"public")));
+
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+app.listen(
+    PORT,
+    () => console.log("Its alive on http://localhost:" + PORT + "")
+)
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+process.on('uncaughtException', err => {
+    console.error(err && err.stack)
+});

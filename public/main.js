9479e1891da84d4c8cb34becac5e03cd5b184b95
Universe <williamsthehack@gmail.com>
2023-01-21 13:44:41 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -20,14 +20,14 @@ function getFeed(type) {
                 '" style="text-decoration: none;" onclick="this.href="https://google.com";""><div class="item" "><p class="link" style="margin: 2px; color: white"; text-decoration: none;>'
                 +element.content+'</p><img src=http://stardash.de:2000/image/'
                 +element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px">'
-                +`<p style='color: white; font-family: nunito'>comments: `+element.comments+` votes: `+element.votes+`</p>`
+                +`<p style='color: white; font-family: nunito; text-align: right'>comments: `+element.comments+` votes: `+element.votes+`</p>`
                 +'</div> </a><hr class = "hr">'
               
             } else {
                 feed+=' <a href="http://stardash.de:7788/post/'+element.post_id+
                 '" style="text-decoration: none;" onclick="this.href="https://google.com";""><div class="item" "><p class="link" style="margin: 2px; color: white"; text-decoration: none;>'
                 +element.content+'</p>'
-                +`<p style='color: white; font-family: nunito'>comments: `+element.comments+` votes: `+element.votes+`</p>`
+                +`<p style='color: white; font-family: nunito; text-align: right'>comments: `+element.comments+` votes: `+element.votes+`</p>`
                 +'</div> </a><hr class = "hr">'
         
             }

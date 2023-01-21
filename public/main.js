048350e91fb810a5ca64237fa3b5c3529f70e043
Universe <williamsthehack@gmail.com>
2023-01-21 13:42:30 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -19,7 +19,9 @@ function getFeed(type) {
                 feed+=' <a href="http://stardash.de:7788/post/'+element.post_id+
                 '" style="text-decoration: none;" onclick="this.href="https://google.com";""><div class="item" "><p class="link" style="margin: 2px; color: white"; text-decoration: none;>'
                 +element.content+'</p><img src=http://stardash.de:2000/image/'
-                +element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px"><br></div> </a><hr class = "hr">'
+                +element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px"><br>'
+                +`<p style='color: white; font-family: nunito'>comments: `+element.comments+` votes: `+element.votes+`</p>`
+                +'</div> </a><hr class = "hr">'
               
             } else {
                 feed+=' <a href="http://stardash.de:7788/post/'+element.post_id+

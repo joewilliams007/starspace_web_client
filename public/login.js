c55da40c93dd50522724da0346efdc02b8e8c8dd
Universe <williamsthehack@gmail.com>
2023-01-21 18:56:17 +0100

    up

---
+++ public/login.js
@@ -0,0 +1,16 @@
+function submit() {
+    const username = document.getElementById('username').value
+    const password = document.getElementById('password').value
+
+    if (username.length < 1) {
+        return alert("enter your username")
+    }
+    if (password.length < 5) {
+        return alert("enter your password")
+    }
+
+   // setCookie("username", username, 30);
+
+    document.getElementById('message').value = ""
+    document.getElementById('password').value = ""
+}

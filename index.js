ec5aee2a9d22f9a6b1c57036ef6c33410ae4a2de
Universe <williamsthehack@gmail.com>
2023-01-21 04:47:11 +0100

    up

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/post/:id?", (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/post.html'));
 })
 
 process.on('uncaughtException', err => {

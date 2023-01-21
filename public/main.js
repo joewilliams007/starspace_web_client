dbb84e2a625aec68c3a8004404cb36ef21c31586
Universe <williamsthehack@gmail.com>
2023-01-21 04:20:08 +0100

    (function( w, d ) {        w.addEventListener( 'scroll', function() {     this.alert("set")       if ( w.innerHeight + w.scrollY > d.height - 300 ) {                console.log("I need to load some more content here…");          this.alert("load")       }          });        })( window, document );

--- public/main.js
+++ public/main.js
@@ -1,17 +1,24 @@
 function loaded() {
     // alert('Please leave this page');
-    getFeed();
+    getFeed("latest");
 }
 
-function getFeed() {
-    fetch("http://stardash.de:2000/feed/latest")
+
+
+function getFeed(type) {
+    fetch("http://stardash.de:2000/feed/"+type)
     .then(response => response.json())
     .then((response) => {
         console.log(response)
 
         var feed;
         response.feed.forEach(element => {
-            feed+='<div class="item"><p class="textW">'+element.content+'</p></div>'
+            if (element.image == 1) {
+                feed+='<hr class = "hr"><div class="item"><p style="margin: 2px; color: white"; >'+element.content+'</p><img src=http://stardash.de:2000/image/'+element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px"><br></div>'
+              
+            } else {
+                feed+='<hr class="hr"><div class="item"><p style="margin: 2px; color: white" >'+element.content+'</p></div>'
+            }
         });
 
         document.getElementById("feed").innerHTML = feed.replace("undefined","");
@@ -20,11 +27,15 @@ function getFeed() {
 }
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        document.getElementById("openbtn").style.visibility = "hidden"
+
   }
   
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("openbtn").style.visibility = "visible"
   }

9a4d78a5b7dd0cfc7d0ca6de368744d5fceeeade
Universe <williamsthehack@gmail.com>
2023-01-21 12:39:13 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -22,9 +22,10 @@ function getFeed(type) {
                 +element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px"><br></div> </a><hr class = "hr">'
               
             } else {
-                feed+='<a href="http://stardash.de:7788/post/'
-                +element.post_id+'" style="text-decoration: none;" onclick="this.href="https://google.com";""><div class="item" ><p style="margin: 2px; color: white; text-decoration: none;" class="link"><hr class = "hr">'
-                +element.content+'</p></div></a>'
+                feed+=' <a href="http://stardash.de:7788/post/'+element.post_id+
+                '" style="text-decoration: none;" onclick="this.href="https://google.com";""><div class="item" "><p class="link" style="margin: 2px; color: white"; text-decoration: none;>'
+                +element.image_path.replaceAll(" ","SPACESYMBOL")+' style="width: 100%; max-height: auto; border-radius: 10px"><br></div> </a><hr class = "hr">'
+        
             }
         });
 

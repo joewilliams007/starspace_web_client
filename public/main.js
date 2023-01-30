7b97eb3137c168826494ef24c29b4d59e24c3837
Universe <williamsthehack@gmail.com>
2023-01-30 15:30:51 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -69,7 +69,7 @@ function checkCookie() {
             console.log(response)
             if (response.success == true) {
                 if (response.image == 1) {
-                    document.getElementById("profile_picture").src = response.image_path;
+                    document.getElementById("profile_picture").src = "http://stardash.de:2000/image/"+response.image_path;
                 }
             }
         })
